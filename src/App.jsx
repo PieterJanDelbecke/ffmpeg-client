097b987453c1cef472803896d7d998fa362dfeb5
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-30 16:08:41 +1000

    sort out merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,156 +1,304 @@
-import { useRef, useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Slider from "rc-slider";
+import TrimmerSlider from "./components/trimmerSlider";
+import axios from "axios";
+import { useEffect, useRef, useState, useCallback } from "react";
+import Cropper from "react-cropper";
+import "rc-slider/assets/index.css";
+import "cropperjs/dist/cropper.css";
 import "./App.css";
 
 function App() {
-  const sliderContainerRef = useRef(null);
-  const sliderDragRef = useRef(null);
-  const leftHandleRef = useRef(null);
-  const rightHandleRef = useRef(null);
+  const [url, setUrl] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [screenShot, setScreenShot] = useState();
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(100);
+  const [currentAspectRatioSelector, setCurrentAspectRatioSelector] =
+    useState();
+  const [savedAspectRatios, setSavedAspectRatios] = useState({
+    facebook: {},
+    instagram: {},
+    twitter: {},
+    linkedIn: {},
+    tiktok: {},
+  });
 
-  const [range, setRange] = useState([0, 100]);
+  const [instagramLimit, setInstagramLimit] = useState();
 
-  const cleanUp = () => {
-    window.document.onmousemove = null;
-    window.document.onmouseup = null;
-  };
+  const marks =
+    instagramLimit && instagramLimit <= 100
+      ? {
+          [instagramLimit]: "instagram",
+        }
+      : {};
 
-  const getPercentage = (val) => {
-    const containerWidth = sliderContainerRef.current?.clientWidth;
-    return (val / containerWidth) * 100;
-  };
-
-  const setRangeHelper = () => {
-    const width = sliderDragRef.current?.clientWidth;
-    const from = getPercentage(sliderDragRef.current?.offsetLeft);
-    const to = getPercentage(width + sliderDragRef.current?.offsetLeft);
-    setRange([from, to]);
-  };
+  const videoRef = useRef(null);
+  const cropperRef = useRef(null);
 
   useEffect(() => {
-    if (
-      leftHandleRef.current &&
-      rightHandleRef.current &&
-      sliderContainerRef.current
-    ) {
-      sliderDragRef.current.style.left =
-        getPercentage(leftHandleRef.current.offsetLeft) + "%";
-      sliderDragRef.current.style.right =
-        getPercentage(
-          sliderContainerRef.current.clientWidth -
-            rightHandleRef.current.offsetLeft -
-            rightHandleRef.current.clientWidth
-        ) + "%";
+    if (videoRef?.current?.duration) {
+      const mark = (100 / videoRef?.current?.duration) * 2 + start;
+      mark <= 100 && setInstagramLimit(mark);
     }
-  }, [leftHandleRef.current, rightHandleRef.current]);
+  }, [videoRef?.current?.duration, start]);
 
-  const handleLeftDrag = (event) => {
-    event.preventDefault();
-    const intialStartLeft = leftHandleRef.current.offsetLeft;
-
-    const drag = (e) => {
-      e.preventDefault();
-      let left = intialStartLeft + (e.clientX - event.clientX);
-      const rigthStarts =
-        rightHandleRef.current.offsetLeft - leftHandleRef.current.clientWidth;
-      if (left <= 0) {
-        left = 0;
-      } else if (left >= rigthStarts) {
-        left = rigthStarts;
-      }
-      sliderDragRef.current.style.left = getPercentage(left) + "%";
-      leftHandleRef.current.style.left = getPercentage(left) + "%";
-      setRangeHelper();
-    };
-
-    window.document.onmousemove = drag;
-    window.document.onmouseup = cleanUp;
+  const aspectRatios = {
+    facebook: ["9 / 16", "16 / 9", "1 / 1"],
+    instagram: ["9 / 16", "16 / 9", "1 / 1", "4 / 5"],
+    twitter: ["1 / 1", "2 / 1"],
+    linkedIn: ["9 / 16", "16 / 9", "1 / 1"],
+    tiktok: ["9 / 16"],
   };
 
-  const handleRightDrag = (event) => {
-    event.preventDefault();
-    const containerWidth = sliderContainerRef.current.clientWidth;
-    const intialStartRight =
-      rightHandleRef.current.offsetLeft + rightHandleRef.current.clientWidth;
-
-    const drag = (e) => {
-      e.preventDefault();
-      let right =
-        containerWidth - (intialStartRight + (e.clientX - event.clientX));
-      const leftEnds =
-        containerWidth -
-        leftHandleRef.current.offsetLeft -
-        (leftHandleRef.current.clientWidth +
-          rightHandleRef.current.clientWidth);
-      if (right <= 0) {
-        right = 0;
-      } else if (right >= leftEnds) {
-        right = leftEnds;
-      }
-      sliderDragRef.current.style.right = getPercentage(right) + "%";
-      rightHandleRef.current.style.right = getPercentage(right) + "%";
-      setRangeHelper();
-    };
-
-    window.document.onmousemove = drag;
-    window.document.onmouseup = cleanUp;
+  const handleScreenShot = () => {
+    if (videoRef?.current) setScreenShot(takeSnapshot(videoRef?.current));
   };
 
-  const handleDrag = (event) => {
+  const changeVidSeek = (value) => {
+    const video = videoRef?.current;
+    if (!video || isNaN(video.duration)) return;
+    videoRef.current.currentTime = videoLengthMapping(video.duration, value);
+  };
+
+  const videoLengthMapping = (duration, value) => {
+    const sector = duration / 100;
+    return sector * value;
+  };
+
+  const takeSnapshot = (video) => {
+    const canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    return canvas.toDataURL("image/jpeg", 0.5);
+  };
+
+  const getMappedRange = (duration, start, end) => {
+    const mappedStart = videoLengthMapping(duration, start);
+    const mappedend = videoLengthMapping(duration, end);
+    return [mappedStart, mappedend];
+  };
+
+  const handleSliderChange = (sliderValue) => {
+    console.log(sliderValue)
+    const [sliderStart, sliderEnd] = sliderValue;
+    if (start !== sliderStart) {
+      changeVidSeek(sliderStart);
+      setStart(sliderStart);
+    } else if (end !== sliderEnd) {
+      changeVidSeek(sliderEnd);
+      setEnd(sliderEnd);
+    }
+  };
+
+  const handleAspectRatioSelect = (key, value) => {
+    cropperRef?.current?.cropper?.reset();
+    setCurrentAspectRatioSelector([key, value]);
+    cropperRef?.current?.cropper?.setAspectRatio(eval(value));
+  };
+
+  const saveAspectRatio = () => {
+    if (!currentAspectRatioSelector) return;
+    const [key, value] = currentAspectRatioSelector;
+    const cropData = cropperRef?.current?.cropper.getData();
+    setSavedAspectRatios((prevSavedAspectRatios) => ({
+      ...prevSavedAspectRatios,
+      [key]: {
+        [value]: cropData,
+      },
+    }));
+  };
+
+  const handleFileSelected = async (event) => {
     event.preventDefault();
-    const initialStart = sliderDragRef.current.offsetLeft;
-    const dragElemWidth = sliderDragRef.current.clientWidth;
-    const prev = event.clientX;
-    const drag = (e) => {
-      e.preventDefault();
-      let boundry = sliderContainerRef.current.clientWidth - dragElemWidth;
-      let positionLeft = initialStart + e.clientX - event.clientX;
-      let positionRight =
-        sliderContainerRef.current.clientWidth - (positionLeft + dragElemWidth);
-      if (positionLeft <= 0) {
-        positionLeft = 0;
-        positionRight = boundry;
+    const postVideo = await axios.post(
+      "http://localhost:4000/video/transcode",
+      { video: event.target.files[0] },
+      {
+        headers: {
+          "content-Type": "multipart/form-data",
+        },
       }
-      if (positionRight <= 0) {
-        positionRight = 0;
-        positionLeft = boundry;
+    );
+    setUrl(postVideo.data.path);
+    setFileName(postVideo.data.fileName);
+    handleScreenShot();
+  };
+
+  const handleTrim = async (event) => {
+    const videoDuration = videoRef?.current?.duration;
+    const [trimStart, trimEnd] = getMappedRange(videoDuration, start, end);
+    const duration = trimEnd - trimStart;
+
+    const postVideo = await axios.post(
+      "http://localhost:4000/video/trim",
+      {
+        start: trimStart,
+        duration,
+        fileName,
+      },
+      {
+        headers: {
+          "content-Type": "application/json",
+        },
       }
+    );
+    setUrl(postVideo.data.path);
+    setFileName(postVideo.data.fileName);
+    handleScreenShot();
+  };
 
-      sliderDragRef.current.style.left = getPercentage(positionLeft) + "%";
-      leftHandleRef.current.style.left = getPercentage(positionLeft) + "%";
-      sliderDragRef.current.style.right = getPercentage(positionRight) + "%";
-      rightHandleRef.current.style.right = getPercentage(positionRight) + "%";
-      setRangeHelper();
-    };
+  const handleCrop = async () => {
+    try {
+      const cropperObject = cropperRef?.current;
+      const cropData = cropperObject.cropper.getData();
+      const cropPost = await axios.post(
+        "http://localhost:4000/video/crop",
+        {
+          fileName,
+          ...cropData,
+        },
+        {
+          headers: {
+            "content-Type": "application/json",
+          },
+        }
+      );
+      setUrl(cropPost.data.path);
+      setFileName(cropPost.data.fileName);
+      handleScreenShot();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    window.document.onmousemove = drag;
-    window.document.onmouseup = cleanUp;
+  const handleTrimAndCrop = async () => {
+    try {
+      const videoDuration = videoRef?.current?.duration;
+      const [trimStart, trimEnd] = getMappedRange(videoDuration, start, end);
+      const duration = trimEnd - trimStart;
+      const cropperObject = cropperRef?.current;
+      const cropData = cropperObject.cropper.getData();
+      const trimCropVideo = await axios.post(
+        "http://localhost:4000/video/trimcrop",
+        {
+          fileName,
+          start: trimStart,
+          duration,
+          ...cropData,
+        },
+        {
+          headers: {
+            "content-Type": "application/json",
+          },
+        }
+      );
+      setUrl(trimCropVideo.data.path);
+      setFileName(trimCropVideo.data.fileName);
+      handleScreenShot();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <>
-      <div style={{ margin: 20 }}>
-        <div ref={sliderContainerRef} className="slider_track">
-          <div
-            ref={sliderDragRef}
-            onMouseDown={handleDrag}
-            className="slider_drag"
-          ></div>
-          <div
-            ref={leftHandleRef}
-            onMouseDown={handleLeftDrag}
-            className="handle left_handle"
-          ></div>
-          <div
-            ref={rightHandleRef}
-            onMouseDown={handleRightDrag}
-            className="handle right_handle"
-          ></div>
-        </div>
-        <div>
-          {range[0].toFixed(2)}-{range[1].toFixed(2)}
-        </div>
+    <div
+      style={{
+        padding: 20,
+      }}
+    >
+      <div>
+        <input type="file" name="file" onChange={handleFileSelected} />
       </div>
-    </>
+      <video
+        ref={videoRef}
+        crossOrigin={"Anonymous"}
+        onLoad={handleScreenShot}
+        onSeeked={handleScreenShot}
+        src={url}
+        width={800}
+        height={800}
+        controls
+      />
+      <TrimmerSlider onChange={handleSliderChange}/>
+      {/* <Slider
+        range
+        marks={marks}
+        allowCross={false}
+        step={0.001}
+        style={{
+          height: 50,
+          width: 800,
+        }}
+        railStyle={{
+          height: 40,
+        }}
+        trackStyle={{
+          opacity: 0,
+        }}
+        handleStyle={{
+          height: 50,
+          borderRadius: 5,
+          width: 10,
+          backgroundColor: "#0000dd",
+        }}
+        defaultValue={[0, 100]}
+        onChange={handleSliderChange}
+      /> */}
+      {screenShot && (
+        <Cropper
+          src={screenShot}
+          style={{ height: 600, width: "100%" }}
+          responsive={true}
+          guides={true}
+          autoCrop={true}
+          dragMode={"move"}
+          viewMode={2}
+          zoomTo={0}
+          minCropBoxHeight={20000}
+          minCropBoxWidth={20000}
+          movable={true}
+          ref={cropperRef}
+        />
+      )}
+      <button onClick={handleTrim}> Trim</button>
+      <button onClick={handleCrop}> Crop</button>
+      <button onClick={handleTrimAndCrop}> Trim & Crop</button>
+      <br />
+      <br />
+      {screenShot && (
+        <>
+          <button onClick={() => cropperRef?.current?.cropper?.reset()}>
+            reset Cropping
+          </button>
+          <button onClick={saveAspectRatio}>Save</button>
+        </>
+      )}
+      <br />
+      <br />
+      {Object.keys(aspectRatios).map((key) => (
+        <div>
+          {key}
+          <br />
+          {aspectRatios[key].map((ar) => (
+            <button
+              className={"show-focus"}
+              style={{
+                backgroundColor: savedAspectRatios?.[key]?.[ar] ? "red" : "",
+              }}
+              onClick={() => handleAspectRatioSelect(key, ar)}
+            >
+              {ar}
+            </button>
+          ))}
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
   );
 }
 
---
+++ src/components/trimmerSlider.jsx
@@ -0,0 +1,155 @@
+import { useRef, useEffect, useState } from "react";
+
+function TrimmerSlider({onChange}) {
+  const sliderContainerRef = useRef(null);
+  const sliderDragRef = useRef(null);
+  const leftHandleRef = useRef(null);
+  const rightHandleRef = useRef(null);
+
+  const [range, setRange] = useState([0, 100]);
+
+  const cleanUp = () => {
+    window.document.onmousemove = null;
+    window.document.onmouseup = null;
+  };
+
+  const getPercentage = (val) => {
+    const containerWidth = sliderContainerRef.current?.clientWidth;
+    return (val / containerWidth) * 100;
+  };
+
+  const setRangeHelper = () => {
+    const width = sliderDragRef.current?.clientWidth;
+    const from = (getPercentage(sliderDragRef.current?.offsetLeft)).toFixed(3);
+    const to = (getPercentage(width + sliderDragRef.current?.offsetLeft)).toFixed(3);
+    setRange([from, to]);
+    onChange([Number(from), Number(to)])
+  };
+  // useEffect(() => {
+  //   console.log(sliderDragRef.current?.clientWidth);
+  // }, [sliderDragRef.current?.clientWidth]);
+
+  useEffect(() => {
+    if (
+      leftHandleRef.current &&
+      rightHandleRef.current &&
+      sliderContainerRef.current
+    ) {
+      sliderDragRef.current.style.left =
+        getPercentage(leftHandleRef.current.offsetLeft) + "%";
+      sliderDragRef.current.style.right =
+        getPercentage(
+          sliderContainerRef.current.clientWidth -
+            rightHandleRef.current.offsetLeft
+        ) + "%";
+    }
+  }, [leftHandleRef.current, rightHandleRef.current]);
+
+  const handleLeftDrag = (event) => {
+    event.preventDefault();
+    const intialStartLeft = leftHandleRef.current.clientLeft;
+    const drag = (e) => {
+      e.preventDefault();
+      let left =
+        e.clientX - intialStartLeft - leftHandleRef.current.clientWidth -9 ;
+      const rigthStarts =
+        rightHandleRef.current.offsetLeft - leftHandleRef.current.clientWidth;
+      if (left <= 0) {
+        left = 0;
+      } else if (left >= rigthStarts) {
+        left = rigthStarts;
+      }
+      sliderDragRef.current.style.left = getPercentage(left) + "%";
+      leftHandleRef.current.style.left = getPercentage(left) + "%";
+      setRangeHelper();
+    };
+
+    window.document.onmousemove = drag;
+    window.document.onmouseup = cleanUp;
+  };
+
+  const handleRightDrag = (event) => {
+    event.preventDefault();
+    const dragWidth = sliderContainerRef.current.clientWidth;
+    const intialStartRight = dragWidth - rightHandleRef.current.offsetLeft;
+    const offsetLeft = rightHandleRef.current.offsetLeft;
+    const drag = (e) => {
+      e.preventDefault();
+      let right = offsetLeft - e.clientX + intialStartRight;
+      const leftEnds =
+        dragWidth -
+        leftHandleRef.current.offsetLeft -
+        rightHandleRef.current.clientWidth * 2;
+      if (right <= 0) {
+        right = 0;
+      } else if (right >= leftEnds) {
+        right = leftEnds;
+      }
+      sliderDragRef.current.style.right = getPercentage(right) + "%";
+      rightHandleRef.current.style.right = getPercentage(right) + "%";
+      setRangeHelper();
+    };
+
+    window.document.onmousemove = drag;
+    window.document.onmouseup = cleanUp;
+  };
+
+  const handleDrag = (event) => {
+    event.preventDefault();
+    const initialStart = sliderDragRef.current.offsetLeft;
+    const dragElemWidth = sliderDragRef.current.clientWidth;
+    const prev = event.clientX;
+    const drag = (e) => {
+      e.preventDefault();
+      let positionLeft = initialStart + e.clientX - prev;
+      let positionRight =
+        sliderContainerRef.current.clientWidth - (positionLeft + dragElemWidth);
+      if (positionLeft <= 0) {
+        positionLeft = 0;
+        positionRight = sliderContainerRef.current.clientWidth - dragElemWidth;
+      }
+      if (positionRight <= 0) {
+        positionRight = 0;
+        positionLeft = sliderContainerRef.current.clientWidth - dragElemWidth;
+      }
+
+      sliderDragRef.current.style.left = getPercentage(positionLeft) + "%";
+      leftHandleRef.current.style.left = getPercentage(positionLeft) + "%";
+      sliderDragRef.current.style.right = getPercentage(positionRight) + "%";
+      rightHandleRef.current.style.right = getPercentage(positionRight) + "%";
+      setRangeHelper();
+    };
+
+    window.document.onmousemove = drag;
+    window.document.onmouseup = cleanUp;
+  };
+
+  return (
+    <>
+      <div>
+        <div ref={sliderContainerRef} className="slider_track">
+          <div
+            ref={sliderDragRef}
+            onMouseDown={handleDrag}
+            className="slider_drag"
+          ></div>
+          <div
+            ref={leftHandleRef}
+            onMouseDown={handleLeftDrag}
+            className="handle left_handle"
+          ></div>
+          <div
+            ref={rightHandleRef}
+            onMouseDown={handleRightDrag}
+            className="handle right_handle"
+          ></div>
+        </div>
+        <div>
+          {range[0]}-{range[1]}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TrimmerSlider;
